3e1a48a63913e2ea878279501035054ed29b2328
vishal <vishalkvd2005@gmail.com>
2021-09-26 12:18:07 +0530

    java codes

---
+++ First.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+//class second  -->If you not write public keyword then it totally fine different class name and file name .
+public class First //But when you write public keyword then it must be file name and class name is same.
+{
+public static void main(String args[])
+{
+System.out.println("vishal singh");
+}
+}
+
+//String[] arg
+//String[] args
+//String arg[]
+//String args[]
