e6d781cf11e645b6beb8d88a7df7a73081b3e154
vishal <vishalkvd2005@gmail.com>
2021-10-02 11:57:10 +0530

    java codes

---
+++ javaProgram/Unicode.java
@@ -0,0 +1,17 @@
+package javaProgram;
+
+public class Unicode 
+{
+
+	public static void main(String[] args) 
+	{
+		for(char c=0x0900;c<=0x0970;c++)
+		 {
+		    System.out.print(c+" ");
+		}
+	}
+
+}
+
+
+
---
+++ javaProgram/helloWorld.java
@@ -0,0 +1,11 @@
+package javaProgram;
+
+public class helloWorld 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("vishal singh");
+	}
+
+}
