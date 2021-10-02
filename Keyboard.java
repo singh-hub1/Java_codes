3bba435cc6ab633afc136c2f940b5a5e82921201
vishal <vishalkvd2005@gmail.com>
2021-10-02 12:24:31 +0530

    java codes

---
+++ Keyboard.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+public class Keyboard
+{
+public static void main(String []args)
+{
+Scanner s=new Scanner(System.in);
+//int a=s.nextInt();
+//System.out.println("This is integer"+ a);
+
+//Float b=s.nextFloat();
+//System.out.println("This is float"+b);
+
+//Double c=s.nextDouble();
+//System.out.println("This is Double"+c);
+
+//Byte d=s.nextByte();
+//System.out.println("This is byte"+d);
+
+//Short e=s.nextShort();
+//System.out.println("This is short"+e);
+
+//Long f=s.nextLong();
+//System.out.println("This is long "+f);
+
+//Boolean g=s.nextBoolean();
+//System.out.println("This is boolean"+g);
+
+//String x=s.nextLine();
+//System.out.println(x);
+
+String y=s.next();
+System.out.println(y);
+
+System.out.println(y.getClass().getName());//This will display data type of the variable.
+}
+}
